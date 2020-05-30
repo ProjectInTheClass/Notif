347e4e1194c62d9049ab277007f6e8002951c85e
2016024911 <z00z222@hanyang.ac.kr>
2020-05-31 00:48:29 +0900

    tab bar icon added & homeTab's autolayout and shadow effect are changed & now hometab just shows cards that processed before 1 day.

--- Noti/HistoryViewController.swift
+++ Noti/HistoryViewController.swift
@@ -31,28 +31,28 @@ class HistoryViewController: UIViewController, UITableViewDataSource {
     func numberOfSections(in tableView: UITableView) -> Int {
         
        
-        return Array(Set( self.cardsViewController.cards.map{$0.formattedDate})).count
+        return Array(Set( self.cardsViewController.cards.map{$0.historyFormattedDate})).count
     }
     
     func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        return Array(Set(self.cardsViewController.cards.map{$0.formattedDate})).sorted().reversed()[section]
+        return Array(Set(self.cardsViewController.cards.map{$0.historyFormattedDate})).sorted().reversed()[section]
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         if(channelsDataSource.allTags.count == 0 ){
                    return 1
         }
-        let date = Array(Set(self.cardsViewController.cards.map{$0.formattedDate})).sorted().reversed()[section]
-        return self.cardsViewController.cards.filter{$0.formattedDate == date}.count
+        let date = Array(Set(self.cardsViewController.cards.map{$0.historyFormattedDate})).sorted().reversed()[section]
+        return self.cardsViewController.cards.filter{$0.historyFormattedDate == date}.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         //home 화면에서 제공되던 card 셀들
         let cardCell = tableView.dequeueReusableCell(withIdentifier: "customCell", for: indexPath) as! HomeTableViewCell
         //날짜별로 정렬하기 위해서 추가, 카드들을 날짜별로 묶어줄 수 있도록 각 섹션의 날짜가 저장된 변수
-        let date = Array(Set(self.cardsViewController.cards.map{$0.formattedDate})).sorted().reversed()[indexPath.section]
+        let date = Array(Set(self.cardsViewController.cards.map{$0.historyFormattedDate})).sorted().reversed()[indexPath.section]
         //cardsViewController.cards를 돌면서 각 날짜에 맞는 card를 필터를 걸어서 찾아내기 위한 변수
-        let nowCell = self.cardsViewController.cards.filter{$0.formattedDate==date}[indexPath.row]
+        let nowCell = self.cardsViewController.cards.filter{$0.historyFormattedDate==date}[indexPath.row]
         //tag를 추가했을 때 생기는 cell, 기존의 card 구조체에서 제목과 시간만 저장됨
         let tagCell = tableView.dequeueReusableCell(withIdentifier: "tagCell", for: indexPath) as! tagTableViewCell
         
@@ -66,7 +66,7 @@ class HistoryViewController: UIViewController, UITableViewDataSource {
             cardCell.titleLabel.text = nowCell.title
             cardCell.sourceLabel.text = nowCell.source
             //        cell.cellView.layer.borderWidth = 1
-            cardCell.dateLabel.text = nowCell.formattedDate
+            cardCell.dateLabel.text = nowCell.historyFormattedDate
             cardCell.cellView.layer.shadowColor = UIColor.black.cgColor
             cardCell.cellView.layer.shadowOffset = CGSize(width: 0, height: 2) //반경
             cardCell.cellView.layer.shadowRadius = 2 // 반경?

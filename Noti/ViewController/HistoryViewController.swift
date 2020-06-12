b6de0cb4d2ff0340d52d093eebbbd0cfbdfcff30
ROOT <junroot0909@gmail.com>
2020-06-12 16:05:48 +0900

    fix Segway in HistoryView

--- Noti/ViewController/HistoryViewController.swift
+++ Noti/ViewController/HistoryViewController.swift
@@ -179,6 +179,7 @@ extension HistoryViewController: UITableViewDelegate, UITableViewDataSource {
                 destination.source = cell.sourceLabel.text
                 destination.date = cell.dateLabel.text
                 destination.back2 = title
+                print("history" + cards[indexPath.row].url)
                 destination.url = cards[indexPath.row].url
                 //                print("!!!!!"+cards[indexPath.row].url)
                 destination.json = cards[indexPath.row].json

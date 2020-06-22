d7dc4c7d94f1e006c91d0730767e3fcd08a07606
jsj7878 <61611131+jsj7878@users.noreply.github.com>
2020-06-22 15:58:05 +0900

    tags with coredata

--- Noti/coreDataManager.swift
+++ Noti/coreDataManager.swift
@@ -223,13 +223,17 @@ class CoreDataManager{
             }
             }
         }
+    }
+    func removeTag(object : NSManagedObject){
+        self.context?.delete(object)
+        do{
+            try self.context!.save()
+        }catch{
+            fatalError("fetch error!")
+        }
         
-}
-    /*func httpRequest(_sender : Any){
-        let api = strUrl + " json"
-        let encoding = api.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)
-        let url = URL(string: encoding!)
-    }*/
+    }
+
     func getUpdated()->[LastUpdated]{
         var lastUpdated = [LastUpdated]()
         let fetchRequest : NSFetchRequest<LastUpdated> = LastUpdated.fetchRequest()
@@ -286,9 +290,6 @@ class CoreDataManager{
             CoreDataManager.shared.saveUpdated(date: dateFormatter.string(from: dateNow!)){ onSuccess in } //print("saved = \(onSuccess)")
             lastUpdated = CoreDataManager.shared.getUpdated()
         }
-        
-//        let url = URL(string:"https://wdjzl50cnh.execute-api.ap-northeast-2.amazonaws.com/RDS/" + lastUpdated[lastUpdated.count - 1].date!)
-        print(lastUpdated[lastUpdated.count - 1].date)
        let url = URL(string:"https://wdjzl50cnh.execute-api.ap-northeast-2.amazonaws.com/RDS/" + "20-06-11")
         do {
             let data = try Data(contentsOf: url!)
@@ -342,33 +343,6 @@ class CoreDataManager{
         }
         return true;
     }
-    /*init(){
-     
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "yyyy/MM/dd HH:mm"
-        self.saveCards(title:"제18회 임베디드SW경진대회 공고", channelName:  "컴퓨터소프트웨어대학", category: "학사일반게시판", tag:["대회"], time: dateFormatter.date(from: "2020-05-14 01:21")!, color: UIColor.first, isVisited: true, url:"http://cs.hanyang.ac.kr/board/info_board.php?ptype=view&idx=28898&page=1&code=notice", json: ["":""])
-        self.saveCards(title:"2020학년도 2학기 재입학 신청 안내", channelName: "컴퓨터소프트웨어대학", category: "학사일반게시판", tag:[], time: dateFormatter.date(from: "2020-05-17 16:20")!, color: UIColor.first, isVisited: true, url:"http://cs.hanyang.ac.kr/board/info_board.php?ptype=view&idx=28892&page=1&code=notice", json: ["":""])
-        self.saveCards(title:"[KAIST] 2020년 몰입캠프 여름학기 모집", channelName: "컴퓨터소프트웨어대학", category: "학사일반게시판",tag:["모집"], time:  dateFormatter.date(from: "2020-05-17 20:14")!, color: UIColor.first, isVisited: true, url:"http://cs.hanyang.ac.kr/board/info_board.php?ptype=view&idx=28891&page=1&code=notice", json: ["":""])
-        self.saveCards(title:"2020학년도 여름계절학기 수강신청 안내", channelName: "컴퓨터소프트웨어대학", category: "학사일반게시판",tag:["수강신청"], time:  dateFormatter.date(from: "2020-05-11 14:12")!, color: UIColor.first, isVisited: true, url:"http://cs.hanyang.ac.kr/board/info_board.php?ptype=view&idx=28890&page=1&code=notice", json: ["":""])
-        self.saveCards(title:"[NCSOFT] 2020 SUMMER INTERN 공개모집 (~5/21)", channelName: "컴퓨터소프트웨어대학", category: "취업정보게시판",tag: ["모집"], time:  dateFormatter.date(from: "2020-05-17 18:22")!, color: UIColor.second, isVisited: false, url:"http://cs.hanyang.ac.kr/board/job_board.php?ptype=view&idx=28897&page=1&code=job_board", json: ["":""])
-        self.saveCards(title:"파이썬마스터 자격검정 안내", channelName: "컴퓨터소프트웨어대학", category: "취업정보게시판", tag:[], time:  dateFormatter.date(from: "2020-05-14 12:12")!, color: UIColor.second, isVisited: false, url:"http://cs.hanyang.ac.kr/board/job_board.php?ptype=view&idx=28896&page=1&code=job_board", json: ["":""])
-        self.saveCards(title:"2020년 상반기 KB국민은행 신입행원(L1) 수시채용", channelName: "컴퓨터소프트웨어대학", category: "취업정보게시판",tag:["채용"], time:  dateFormatter.date(from: "2020-05-18 10:45")!, color: UIColor.second, isVisited: false, url:"http://cs.hanyang.ac.kr/board/job_board.php?ptype=view&idx=28895&page=1&code=job_board", json: ["":""])
-        self.saveCards(title:"2020-2학기 1차 국가근로장학금 학생신청기간 안내", channelName: "포털", category: "장학게시판",tag:["장학금"], time:  dateFormatter.date(from: "2020-05-18 17:50")!, color: UIColor.third, isVisited: true, url:"https://portal.hanyang.ac.kr/GjshAct/findGongjisahangs.do?pgmId=P308200&menuId=M006263&tk=0be29593626429dfc3f1b618045bc8172b86832df0d333bc0f5db47199b9028a", json: ["gongjiSeq":"15689"])
-        self.saveCards(title:"대운동장 인조잔디구장 및 지하주차장 사용 안내", channelName: "포털", category: "학사게시판", tag:[], time:  dateFormatter.date(from: "2020-05-18 14:40")!, color: UIColor.fourth, isVisited: false, url:"https://portal.hanyang.ac.kr/GjshAct/findGongjisahangs.do?pgmId=P308200&menuId=M006263&tk=0be29593626429dfc3f1b618045bc8172b86832df0d333bc0f5db47199b9028a", json: ["gongjiSeq":"15688"])
-        self.saveCards(title:"2020년도 상반기 울산광역시 대학생 학자금대출 이자지원 사업 신청 안내", channelName: "포털", category: "장학게시판", tag:[], time:  dateFormatter.date(from: "2020-05-18 11:10")!, color: UIColor.third, isVisited: false, url:"https://portal.hanyang.ac.kr/GjshAct/findGongjisahangs.do?pgmId=P308200&menuId=M006263&tk=0be29593626429dfc3f1b618045bc8172b86832df0d333bc0f5db47199b9028a" , json: ["gongjiSeq":"15686"])
-        self.saveCards(title:"2020-1학기 국가장학금1유형 지급(3차 지급실행) 예정 안내", channelName: "포털", category: "장학게시판", tag:["장학금"], time: dateFormatter.date(from: "2020-05-18 10:20")!, color:UIColor.third, isVisited: false, url:"https://portal.hanyang.ac.kr/GjshAct/findGongjisahangs.do?pgmId=P308200&menuId=M006263&tk=0be29593626429dfc3f1b618045bc8172b86832df0d333bc0f5db47199b9028a", json: ["gongjiSeq":"15685"])
-        self.saveCards(title:"장학", channelName: "", category: "", tag :[], time:dateFormatter.date(from : "2020-05-18 17:10")!, color:UIColor.first, isVisited: false, url:"", json:["":""])
-        dateFormatter.dateFormat = "yyyy/MM/dd"
-        self.saveChannels(title: "전체",subtitle: "전체", category: "", color: .sourceFont, channelTags: ["대회","모집"])
-        self.saveChannels(title: "학사게시판",subtitle: "학사", category:  "포털", color: .fourth, channelTags: [] )
-        self.saveChannels(title: "장학게시판",subtitle: "장학", category:  "포털",color: .third,  channelTags: ["장학금"])
-        self.saveChannels(title: "학사일반게시판",subtitle: "학사일반", category: "컴퓨터소프트웨어대학",color: .first, channelTags: ["대회","모집"])
-        self.saveChannels(title: "취업정보게시판",subtitle: "취업정보", category: "컴퓨터소프트웨어대학",color: .second, channelTags: ["모집","채용"])
-        self.saveTags(name: "대회", time: dateFormatter.date(from: "2020-05-12")!)
-        self.saveTags(name: "모집", time: dateFormatter.date(from: "2020-05-11")!)
-        self.saveTags(name: "채용", time: dateFormatter.date(from: "2020-05-14")!)
-        self.saveTags(name: "장학금", time: dateFormatter.date(from: "2020-05-13")!)
-    }*/
 }
     
 extension CoreDataManager {

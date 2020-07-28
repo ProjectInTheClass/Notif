11056806b6783400375e75f4234bce433f601416
jsj7878 <61611131+jsj7878@users.noreply.github.com>
2020-07-28 13:34:19 +0900

    darkmode add

---
+++ Noti/Token+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  Token+CoreDataClass.swift
+//  Noti
+//
+//  Created by sejin on 2020/07/27.
+//  Copyright © 2020 Junroot. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Token)
+public class Token: NSManagedObject {
+
+}

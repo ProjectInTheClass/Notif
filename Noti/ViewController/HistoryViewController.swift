04c218498d54cb3bb0ab4d34ab6b7403890bdb45
2016024911 <z00z222@hanyang.ac.kr>
2020-09-02 07:06:11 +0900

    temporary

--- Noti/ViewController/HistoryViewController.swift
+++ Noti/ViewController/HistoryViewController.swift
@@ -32,7 +32,7 @@ class HistoryViewController: UIViewController{
     func updateTitle(title: String){
         let longTitleLabel = UILabel()
         longTitleLabel.text = title
-        longTitleLabel.font = .boldSystemFont(ofSize: 27)
+        longTitleLabel.font = .boldSystemFont(ofSize: 25)
         longTitleLabel.sizeToFit()
         longTitleLabel.textColor = .navFont
 
